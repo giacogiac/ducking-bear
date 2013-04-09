2bc04af07f4b99caeb26441bd43eac4587a6a1e0
giacogiac <giacomo.rombaut@gmail.com>
2013-04-09 14:29:32 +0200

    ajout de projets

---
+++ Administration/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using DataLib;
+
+namespace Administration
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LibrairieInutile.EcrireCoucou();
+        }
+    }
+}
---
+++ LibrairieInutile/LibrairieInutile.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DataLib
+{
+    public class LibrairieInutile
+    {
+        public static void EcrireCoucou()
+        {
+            Console.WriteLine("coucou");
+        }
+    }
+}
